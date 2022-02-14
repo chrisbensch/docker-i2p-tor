b4dad9148172871f8b9bee307b241f2c40e3ad90
Chris Bensch <chris.bensch@gmail.com>
2022-02-14 14:54:06 +0000

    Update i2p-tor

--- bin/build.sh
+++ bin/build.sh
@@ -9,14 +9,12 @@ set -e
 PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../"
 cd ${PROJECT_PATH}
 
-sudo docker build --force-rm --no-cache -t divax/i2p:latest .
+sudo docker build --force-rm --no-cache -t divax/i2p:i2p-tor .
 sudo docker volume create i2pd-build
-sudo docker volume create i2pd-data
 sudo docker run \
   -d \
   --mount type=volume,src=i2pd-build,dst=/home/i2pd/ \
-  --mount type=volume,src=i2pd-data,dst=/home/i2pd/data \
-  --name i2pd-build divax/i2p:latest
+  --name i2pd-build divax/i2p:i2p-tor
 
 # update certs
 rm -R certificates
@@ -31,4 +29,3 @@ sudo cp -r -f /var/lib/docker/volumes/i2pd-build/_data/LICENSE ./LICENSE-i2pd
 sudo docker stop i2pd-build
 sudo docker rm i2pd-build
 sudo docker volume rm i2pd-build
-sudo docker volume rm i2pd-data
